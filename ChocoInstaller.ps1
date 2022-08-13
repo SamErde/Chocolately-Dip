2d00be198159710c8fdbe40f27c040d7f3dfda66
Sam Erde <sam.erde@martinspoint.org>
2022-08-12 22:28:49 -0400

    Converting Choco installer to function

--- ChocoInstaller.ps1
+++ ChocoInstaller.ps1
@@ -1,9 +1,16 @@
-# Allow the current PowerShell process to bypass execution policies in order to download and run the chocolatey installer.
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco feature enable -n=allowGlobalConfirmation
+function Run-ChocolateyDip {
+
+    [CmdletBinding()]
+    param(
+      [switch]$InstallApps,
+      [switch]$UpdateApps,
+      [switch]$InstallChocolately
+    )
+
+    choco feature enable -n=allowGlobalConfirmation
 
 # List all of the packages for chocolately to install. Each should be in double quotes followed by a comma.
-$pkgs = @( 
+$Packages = @( 
     "7zip.install",             #7-Zip
     "authy-desktop",            #Authy TFA App
     "azure-cli",                #Azure CLI Tools
@@ -43,21 +50,27 @@ $pkgs = @(
     "youtube-dl"                #YouTube Downloader
     )
 
-    param(
-        [Parameter(Mandatory=$False)]
-        [switch]$install,
-        [switch]$update
-    )
-
-If($install){
+If ($InstallApps) {
     #Install each package that is listed in the multi-line string variable.
-    foreach ($package in $pkgs) { choco install $package }
+    foreach ($package in $Packages) { choco install $package }
 }
 
-If($update){
+If ($UpdateApps) {
     #Update all installed chocolately packages.
     choco upgrade all
 }
 
+If ($InstallChocolately) {
+    # Allow the current PowerShell process to bypass execution policies in order to download and run the chocolatey installer.
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
 
+}
 
+Function CheckChocolateyInstall {
+    # Test for installation of Chocolatey
+}
+
+Run-ChocolateyDip
