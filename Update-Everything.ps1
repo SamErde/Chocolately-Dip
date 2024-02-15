77d0f60d86218f821efa9e5a5999be8f3ae67b46
Sam Erde <20478745+SamErde@users.noreply.github.com>
2024-02-15 15:57:14 -0500

    Add console output

--- Update-Everything.ps1
+++ Update-Everything.ps1
@@ -1,15 +1,21 @@
 function Update-Everything {
     [CmdletBinding()]
-    [Alias("upupdate")]
+    [Alias("update")]
     param ()
 
     # Update all PowerShell modules
+    Write-Host -ForegroundColor Yellow -BackgroundColor Black "Updating PowerShell Modules"
     Update-Module
 
+    # Update PowerShell Help
+    Write-Host -ForegroundColor Yellow -BackgroundColor Black "Updating PowerShell Help"
+    Update-Help -Recurse
+
     # Update all winget packages
+    Write-Host -ForegroundColor Yellow -BackgroundColor Black "Updating Winget Packages"
     winget upgrade --silent --scope user --accept-package-agreements --accept-source-agreements --all
 
     # Upgrade Chocolatey packages
+    Write-Host -ForegroundColor Yellow -BackgroundColor Black "Updating Chocolatey Packages"
     choco upgrade all
-
 }
