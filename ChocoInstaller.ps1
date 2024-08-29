0688dae48bf7996943be1bf0718f03ae37c61da9
Sam Erde <samuel.erde@gmail.com>
2024-08-29 15:48:24 -0400

    Cleanup and add note about archival with reference to PSPreworkout.

--- ChocoInstaller.ps1
+++ ChocoInstaller.ps1
@@ -2,61 +2,54 @@ function Start-ChocolateyDip {
 
     [CmdletBinding()]
     param(
-      [switch]$InstallApps,
-      [switch]$UpdateApps,
-      [switch]$InstallChocolately
+        [switch]$InstallApps,
+        [switch]$UpdateApps,
+        [switch]$InstallChocolately
     )
 
     choco feature enable -n=allowGlobalConfirmation
 
-# List all of the packages for chocolately to install. Each should be in double quotes followed by a comma.
-$Packages = @( 
-    "7zip.install",             #7-Zip
-    "authy-desktop",            #Authy TFA App
-    "azure-cli",                #Azure CLI Tools
-    "azurepowershell",          #PowerShell Module for Azure
-    "curl",                     #curl
-    "dellcommandupdate",        #Dell Command Update
-    "filezilla",                #FileZilla
-    "firefox",                  #Firefox
-    "git.install",              #Git
-    "googlechrome",             #Chrome
-    "lastpass",                 #LastPass
-    "lastpass-for-applications", 
-    "ntfssecurity-psmodule",    #PowerShell Module for NTFS Security
-    "onenote",                  #OneNote 2016
-    "orionsdk",                 #Solarwinds Orion SDK and PowerShell Module
-    "poshadmin",                #PowerShell Admin
-    "powershell-core",          #PowerShell 6.0 Core
-    "putty.install",            #PuTTy
-    "psget",                    #PSGet (NuGet for PowerShell)
-    "rdmfree",                  #Remote Desktop Manager
-    "rsat",                     #Remote Server Admin Tools
-    "rvtools",                  #RVtools
-    "sysinternals",             #SysInternals
-    "treesizefree",             #TreeSize Free
-    "vscode",                   #Visual Studio Code
-    "vscode-powershell",        #PowerShell Extension for VS Code
-    "winscp",                   #WinSCP
-    "youtube-dl"                #YouTube Downloader
+    # List all of the packages for chocolately to install. Each should be in double quotes followed by a comma.
+    $Packages = @(
+        '7zip.install', #7-Zip
+        'azure-cli', #Azure CLI Tools
+        'curl', #curl
+        'filezilla', #FileZilla
+        'firefox', #Firefox
+        'git.install', #Git
+        'googlechrome', #Chrome
+        'lastpass', #LastPass
+        'lastpass-for-applications',
+        'ntfssecurity-psmodule', #PowerShell Module for NTFS Security
+        'poshadmin', #PowerShell Admin
+        'putty.install', #PuTTy
+        'rdmfree', #Remote Desktop Manager
+        'rsat', #Remote Server Admin Tools
+        'rvtools', #RVtools
+        'sysinternals', #SysInternals
+        'treesizefree', #TreeSize Free
+        'vscode', #Visual Studio Code
+        'vscode-powershell', #PowerShell Extension for VS Code
+        'winscp', #WinSCP
+        'youtube-dl'                #YouTube Downloader
     )
 
-If ($InstallApps) {
-    #Install each package that is listed in the multi-line string variable.
-    foreach ($package in $Packages) { choco install $package }
-}
+    If ($InstallApps) {
+        #Install each package that is listed in the multi-line string variable.
+        foreach ($package in $Packages) { choco install $package }
+    }
 
-If ($UpdateApps) {
-    #Update all installed chocolately packages.
-    choco upgrade all
-}
+    If ($UpdateApps) {
+        #Update all installed chocolately packages.
+        choco upgrade all
+    }
 
-If ($InstallChocolately) {
-    # Allow the current PowerShell process to bypass execution policies in order to download and run the chocolatey installer.
-    Set-ExecutionPolicy Bypass -Scope Process -Force
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-}
+    If ($InstallChocolately) {
+        # Allow the current PowerShell process to bypass execution policies in order to download and run the chocolatey installer.
+        Set-ExecutionPolicy Bypass -Scope Process -Force
+        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    }
 
 }
 
--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # Chocolatey-Dip
+
+> [!NOTE]
+> The functionality in Chocolatey-Dip and Win-Dip is being moved to the new [PSPreworkout](https://github.com/SamErde/PSPreworkout) repository.
+
 Use Chocolately, Winget, and the PowerShell gallery to quickly install all of the tools and modules that I need on a new machine build.
 
 ### ChocoInstaller.ps1
-Installs Chocolatey for the current user and then installs or updates all listed apps from the Chocolatey repository.  
+
+Installs Chocolatey for the current user and then installs or updates all listed apps from the Chocolatey repository.
 
 ### WingetInstaller.ps1
+
 Installs the given list of packages from the Winget repository or from the Microsoft Store. (Signing into the Windows Store is not required if using winget 1.3 or higher.)
 
-### InstallPowerShellModules.ps1
-Installs a list of PowerShell modules that are likely to be used by an infrastructure or systems engineer.
+### Install-MyModules.ps1
 
-## To Do
-Replace `choco update` with choco upgrade.
-Wrap in a function.
- - Define install function
- - Define update/upgrade function
- - Set default function
+Installs a list of PowerShell modules that are likely to be used by an infrastructure or systems engineer.
